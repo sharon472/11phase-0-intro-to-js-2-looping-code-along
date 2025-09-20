2d14ddc8bbd77904a28abc1ff74d76f79ec0cd58
Sharon Njoroge <waithirasharon222@gmail>
2025-09-20 15:22:36 +0300

    changes made to js code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+module.exports = {
+  writeCards,
+  countDown
+};
